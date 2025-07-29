e86b2dd7e670cd22b8a4200717263eb2d5509edb
Prathamesh Jadhav <prathameshjadhav0198@gmail.com>
2025-07-29 23:40:32 +0530

    configuration added

--- Backend/src/main/java/com/Jadhav/WebCraft/config/JacksonConfig.java
+++ Backend/src/main/java/com/Jadhav/WebCraft/config/JacksonConfig.java
@@ -1,4 +1,16 @@
 package com.Jadhav.WebCraft.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class JacksonConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+        return mapper;
+    }
 }
