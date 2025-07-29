2f63da2608410af9ff86281b1a0018785a1058fc
Prathamesh Jadhav <prathameshjadhav0198@gmail.com>
2025-07-29 18:22:18 +0530

    setup

---
+++ Backend/src/test/java/com/Jadhav/WebCraft/WebCraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Jadhav.WebCraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
